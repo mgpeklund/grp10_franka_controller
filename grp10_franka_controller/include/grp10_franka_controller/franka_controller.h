cda219ab6d6201dcb041e5b8d284eaf7fc9241f0
Mattias Eklund <mgp.eklund@gmail.com>
2018-10-19 13:49:07 +0200

    all the stuff

---
+++ grp10_franka_controller/include/grp10_franka_controller/franka_controller.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <array>
+#include <memory>
+#include <string>
+
+
+#include <controller_interface/multi_interface_controller.h>
+#include <franka_hw/franka_state_interface.h>
+#include <hardware_interface/robot_hw.h>
+#include <ros/node_handle.h>
+#include <ros/time.h>
+
+#include <franka_hw/franka_cartesian_command_interface.h>
+
+namespace grp10_franka_controller {
+
+class FrankaController
+    : public controller_interface::MultiInterfaceController<franka_hw::FrankaPoseCartesianInterface,
+                                                            franka_hw::FrankaStateInterface> {
+ public:
+  bool init(hardware_interface::RobotHW* robot_hardware, ros::NodeHandle& node_handle) override;
+  void starting(const ros::Time&) override;
+  void update(const ros::Time&, const ros::Duration& period) override;
+
+ private:
+//  franka_hw::FrankaVelocityCartesianInterface* velocity_cartesian_interface_;
+//  std::unique_ptr<franka_hw::FrankaCartesianVelocityHandle> velocity_cartesian_handle_;
+
+  franka_hw::FrankaPoseCartesianInterface* cartesian_pose_interface_;
+  std::unique_ptr<franka_hw::FrankaCartesianPoseHandle> cartesian_pose_handle_;
+  ros::Duration elapsed_time_;
+  std::array<double, 16> initial_pose_{};
+};
+
+} // namespace grp10_franka_controller
